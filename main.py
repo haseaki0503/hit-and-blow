29efa407b17d256f7261598c415f517a9d25bf09
はせあき <haseaki0503@gmail.com>
2014-11-03 21:56:55 +0900

    Fixed issue #2
    exception handling

--- main.py
+++ main.py
@@ -2,22 +2,31 @@
 import random
 
 Answer = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+i = 0
 
 def Initialization():
-	for x in range(1,1000):
-		j = random.randint(0,9)
-		k = random.randint(0,9)
-		tmp = Answer[j]
-		Answer[j] = Answer[k]
-		Answer[k] = tmp
+  for x in range(1,1000):
+    j = random.randint(0,9)
+    k = random.randint(0,9)
+    tmp = Answer[j]
+    Answer[j] = Answer[k]
+    Answer[k] = tmp
 
-def AnswerCheck():
+def InputCheck(GuessInput):
+  if len(GuessInput) != 4:
+    print("Error!!")
+  else:
+    global i
+    i += 1
+    AnswerCheck(GuessInput)
+
+def AnswerCheck(GuessInput):
         global hit
         global blow
 
         hit=0
         blow=0
-        
+
         for x in range(0, 4):
                 if Answer[x] == GuessInput[x]:
                         hit += 1
@@ -27,18 +36,21 @@ def AnswerCheck():
             if Answer[x] == GuessInput[y]:
               blow += 1
               break
+
         blow -= hit
         print("hit= " + str(hit) + " " + "blow= " + str(blow))
-        
+
 #MAIN
+print("Hit and Blow GAME!!")
 Initialization()
 
-for f in range(0, 9):
-	GuessInput = input("Guess===>")
-	AnswerCheck()
+while (1):
+  GuessInput = input("Guess===>")
+  InputCheck(GuessInput)
 
   if hit == 4:
-		print("Congratulations!!")
-		break
-
-print("Answer= " + str(Answer[0])+str(Answer[1])+str(Answer[2])+str(Answer[3]))
+    print("Congratulations! You're WON!! ")
+    break
+  if i == 10:
+    print("You're LOST...Answer is " + str(Answer[0])+str(Answer[1])+str(Answer[2])+str(Answer[3]))
+    break
